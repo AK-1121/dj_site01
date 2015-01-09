1e7d61ce9b7966d9317e8fa0ea4a416396536f72
alex <akmail255@gmail.com>
2015-01-09 20:51:19 +0400

    09-01-2015(2)

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from polls.models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+#class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+#admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets=[
+        ('First block',  {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+        #(None, {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
 
 # Register your models here.
