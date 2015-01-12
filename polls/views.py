3e198c0be8005fbce7a7419786b991373ff22a56
AK-1121 <akmail255@gmail.com>
2015-01-12 09:42:25 +0400

    1234

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,11 @@
-#from django.shortcuts import render
 from django.http import HttpResponse
 #from django.template import RequestContext, loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from polls.models import Question
 
 # Create your views here.
 def index(request):
-    #return HttpResponse("Hello, world. You're at polls index.")
     latest_question_list = Question.objects.order_by('-pub_date')[:4]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
@@ -19,7 +17,9 @@ def index1(request):#test index
     return HttpResponse("Hello, letter[a-d]!")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
