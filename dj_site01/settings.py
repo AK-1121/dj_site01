c37931a6dfcb72b5c059e32830ee955dddef7959
AK-1121 <akmail255@gmail.com>
2015-01-19 09:08:49 +0400

    19-01-2015

--- dj_site01/settings.py
+++ dj_site01/settings.py
@@ -85,3 +85,52 @@ STATIC_URL = '/static/'
 
 #Templates:
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+#about log file for Django:
+#http://stackoverflow.com/questions/5739830/simple-log-to-file-example-for-django-1-3
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            #'filename': SITE_ROOT + "/logfile",
+            'filename':os.path.join(BASE_DIR, 'logfile1'),
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'MYAPP': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+    }
+}
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 #from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 
-from polls.models import Question
+from polls.models import Choice, Question
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 # Create your views here.
 def index(request):
@@ -26,6 +31,36 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    #return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    f=open(os.path.join(BASE_DIR, 'logfile1'),'w+')
+    f.write("+++views-votes:+++\n"+str(p)+"\n")
+    #f.write(("len:",len(p)))
+    f.write(str(p.__dict__))
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+        f.write("\n\nselected_choice:"+str(selected_choice)+" type :"+
+                str(selected_choice.__class__)+"\n\ndict -> "+str(selected_choice.__dict__))
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        #Always return an HttResponseRediret after successfully dealing 
+        #with POST data. This prevents data from being posted twice if a 
+        #user hits the Back button.
+        #return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        HRR = HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        f.write("\n\n\nHttpResponseRedirect: "+str(HRR)+"\n\n"+str(HRR.__dict__))
+        f.write("\n----views - votes----\n")
+        f.close()
+        return HRR
+        
+    
+
 
 
